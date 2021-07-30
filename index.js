799f8acac8400480df7a30cc23f6156ac519d29c
petroDavydov <petrodavydov@gmail.com>
2021-07-30 16:18:50 +0300

    pretier work

--- index.js
+++ index.js
@@ -17,30 +17,27 @@ const refs = {
   body: document.querySelector("body"),
 };
 
-
 const timer = {
   isActive: false,
   start() {
     if (this.isActive) {
       return;
     }
-	this.isActive = true;
+    this.isActive = true;
 
-	this.changeColors = setInterval(()=>{
-		const min = 0;
-		const max = colors.length -1;
-		let rifi = randomIntegerFromInterval(min,max);
-		refs.body.style.background = colors[rifi];
-	},1000)
+    this.changeColors = setInterval(() => {
+      const min = 0;
+      const max = colors.length - 1;
+      let rifi = randomIntegerFromInterval(min, max);
+      refs.body.style.background = colors[rifi];
+    }, 1000);
   },
 
-  stop(){
-	  clearInterval(this.changeColors);
-	  this.isActive = false;
-  }
+  stop() {
+    clearInterval(this.changeColors);
+    this.isActive = false;
+  },
 };
-
 
 refs.startButton.addEventListener("click", timer.start.bind(timer));
 refs.stopButton.addEventListener("click", timer.stop.bind(timer));
-
