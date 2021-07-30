326ad9e589a65055b352f9b4061a419ee6a3b17d
petroDavydov <petrodavydov@gmail.com>
2021-07-30 16:18:08 +0300

    add color-switch

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
 const colors = [
-	'#FFFFFF',
-	'#2196F3',
-	'#4CAF50',
-	'#FF9800',
-	'#009688',
-	'#795548',
-  ];
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
 
-  const randomIntegerFromInterval = (min, max) => {
-	return Math.floor(Math.random() * (max - min + 1) + min);
-  };
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
-  const refs = {
-	  startButton: document.querySelector('data-action="start"'),
-	  stopButton: document.querySelector('data-action="stop"'),
-	  body:document.querySelector('body')
-  };
+const refs = {
+  startButton: document.querySelector('[data-action="start"]'),
+  stopButton: document.querySelector('[data-action="stop"]'),
+  body: document.querySelector("body"),
+};
 
-refs.startButton.addEventListener('click')
-refs.stopButton.addEventListener('click')
+
+const timer = {
+  isActive: false,
+  start() {
+    if (this.isActive) {
+      return;
+    }
+	this.isActive = true;
+
+	this.changeColors = setInterval(()=>{
+		const min = 0;
+		const max = colors.length -1;
+		let rifi = randomIntegerFromInterval(min,max);
+		refs.body.style.background = colors[rifi];
+	},1000)
+  },
+
+  stop(){
+	  clearInterval(this.changeColors);
+	  this.isActive = false;
+  }
+};
+
+
+refs.startButton.addEventListener("click", timer.start.bind(timer));
+refs.stopButton.addEventListener("click", timer.stop.bind(timer));
+
